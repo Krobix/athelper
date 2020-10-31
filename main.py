7dc0e73971a37e0ca0c3df732f84be168365e42c
Krobix <mrzlime@gmail.com>
2020-10-31 16:49:34 -0400

    finished timeloop stuff

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ import pickle
 import os
 import copy
 import traceback
+import secrets
 from discord.ext import tasks, commands
+
+DEVKEY = secrets.token_hex(5)
 
 bot = commands.Bot(command_prefix="at.")
 
@@ -20,6 +23,8 @@ now = datetime.datetime.now()
 m_names = []
 
 h_names = []
+
+FALLBACK_DEV_ID = 229277947733868545
 
 class ATHelperStatusData:
     def __init__(self, mchannel, hchannel, hindex, mindex, devuser):
@@ -36,7 +41,7 @@ def setup():
     os.mkdir("data/chr")
     os.mkdir("data/chr/tables")
     os.mkdir("data/chr/obj")
-    status = ATHelperStatusData(None, None, now.hour, now.month, None)
+    status = ATHelperStatusData(None, None, now.hour, now.month, 0)
 
 async def write_data_to_disk():
     nstatus = copy.copy(status)
@@ -69,7 +74,7 @@ async def set_once_hourly(ctx):
 @bot.command()
 async def setdev(ctx):
     global status
-    if status.devuser == None:
+    if status.devuser == 0:
         status.devuser = ctx.author
         await ctx.send("OK")
     else:
@@ -91,16 +96,25 @@ async def info(ctx, page):
     else:
         ctx.send(f"Error: {page} is not a known info page")
 
-@tasks.loop(minutes=10.0)
+@bot.command(name="eval")
+async def eval_comm(ctx, key, stmt):
+    if key == DEVKEY:
+        out = str(eval(stmt))
+        await ctx.send(out)
+    else:
+        pass
+
 async def check_time():
     global now
-    print("Running check_time")
-    new_now = datetime.datetime.now()
-    status.hour_index = h_names[new_now.hour]
-    status.month_index = m_names[new_now.month]
-    status.once_monthly_channel.edit(name=status.month_index)
-    status.once_hourly_channel.edit(name=status.hour_index)
-    now = new_now
+    while True:
+        #print("Running check_time")
+        new_now = datetime.datetime.now()
+        status.hour_index = h_names[new_now.hour]
+        status.month_index = m_names[new_now.month]
+        await status.once_monthly_channel.edit(name=status.month_index)
+        await status.once_hourly_channel.edit(name=status.hour_index)
+        now = new_now
+        await asyncio.sleep(600)
 
 @bot.event
 async def on_command_error(context, error):
@@ -112,16 +126,27 @@ async def on_command_error(context, error):
     emb.description = f"An error has occured. Please report it to the developer:\n\n```{''.join(traceback.format_exception(type(error), error, error.__traceback__))}```"
     await context.send(embed=emb)
 
-def main():
-    global status, m_names, h_names
+@bot.event
+async def on_ready():
+    global status
+    await bot.wait_until_ready()
+    print(f"devkey={DEVKEY}")
     if not os.path.isdir("data"):
         setup()
     else:
         with open("data/status.bin", "rb") as f:
             status = pickle.load(f)
-            status.devuser = bot.get_user(status.devuser)
-            status.once_hourly_channel = bot.get_channel(status.once_hourly_channel)
-            status.once_monthly_channel = bot.get_channel(status.once_monthly_channel)
+            print(f"devuser id={status.devuser}")
+            status.devuser = bot.get_user(int(status.devuser))
+            print(f"DEVUSER DETECTED:\n\n{status.devuser}")
+            status.once_hourly_channel = bot.get_channel(int(status.once_hourly_channel))
+            status.once_monthly_channel = bot.get_channel(int(status.once_monthly_channel))
+            if status.devuser == None:
+                status.devuser = bot.get_user(FALLBACK_DEV_ID)
+    bot.loop.create_task(check_time())
+
+def main():
+    global status, m_names, h_names
     with open("static/ch_change_names_h", "r") as f:
         h_names = f.read()
         h_names = h_names.strip("\n")
